2bd9a99d14c79dca5a02d3cc6383a1220d19d14c
Steve Le Roy Harris <steve@nourish.je>
2016-03-24 11:56:56 +0000

    Correct bullet type

--- data.lua
+++ data.lua
@@ -10,6 +10,7 @@ return
   direction_count = 4,
   frame_count = inputs.frame_count and inputs.frame_count or 5,
   line_length = inputs.line_length and inputs.line_length or 0,
+  animation_speed = inputs.speed and inputs.speed or 1,
   run_mode = inputs.run_mode and inputs.run_mode or "forward",
   shift = {0.078125, -0.859375},
   axially_symmetrical = false
@@ -25,6 +26,7 @@ return
   direction_count = 4,
   frame_count = inputs.frame_count and inputs.frame_count or 5,
   line_length = inputs.line_length and inputs.line_length or 0,
+  animation_speed = inputs.speed and inputs.speed or 1,
   run_mode = inputs.run_mode and inputs.run_mode or "forward",
   shift = {0.0625, -0.890625},
   axially_symmetrical = false,
@@ -41,6 +43,7 @@ return
   direction_count = 4,
   frame_count = inputs.frame_count and inputs.frame_count or 5,
   line_length = inputs.line_length and inputs.line_length or 0,
+  animation_speed = inputs.speed and inputs.speed or 1,
   run_mode = inputs.run_mode and inputs.run_mode or "forward",
   shift = {1.26563, 0.015625},
   axially_symmetrical = false,
@@ -125,11 +128,11 @@ data:extend(
     stack_size = 10
   },
   {
-    type = "ammo-turret",
+    type = "unit",
     name = "bambi-turret",
     icon = "__base__/graphics/icons/gun-turret.png",
-    flags = {"placeable-player", "player-creation"},
-    order="b-b-d",
+    flags = {"placeable-player", "player-creation", "placeable-off-grid"},
+    order="b-b-b",
     subgroup="enemies",
     minable = {mining_time = 0.5, result = "bambi-turret"},
     max_health = 400,
@@ -143,6 +146,8 @@ data:extend(
     inventory_size = 1,
     automated_ammo_count = 10,
     attacking_speed = 0.5,
+    movement_speed = 0.085,
+    distance_per_frame = 0.15,
     folded_animation = 
     {
       layers =
@@ -150,6 +155,15 @@ data:extend(
         gun_turret_extension{frame_count=1, line_length = 1},
         gun_turret_extension_mask{frame_count=1, line_length = 1},
         gun_turret_extension_shadow{frame_count=1, line_length = 1}
+      }
+    },
+    run_animation = 
+    {
+      layers =
+      {
+        gun_turret_extension{speed=0.5},
+        gun_turret_extension_mask{speed=0.5},
+        gun_turret_extension_shadow{speed=0.5},
       }
     },
     preparing_animation = 
@@ -162,7 +176,6 @@ data:extend(
       }
     },
     prepared_animation = gun_turret_attack{frame_count=1},
-    attacking_animation = gun_turret_attack{},
     folding_animation = 
     { 
       layers = 
@@ -199,26 +212,47 @@ data:extend(
       }
     },
     vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    dying_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    pollution_to_join_attack = 1,
+    distraction_cooldown = 300,
+    vision_distance = 30,
     attack_parameters =
     {
       type = "projectile",
-      ammo_category = "bullet",
       cooldown = 6,
       projectile_creation_distance = 1.39375,
       projectile_center = {0.0625, -0.0875}, -- same as gun_turret_attack shift
       damage_modifier = 2,
-      shell_particle =
+      animation = gun_turret_attack{},
+      ammo_type =
       {
-        name = "shell-particle",
-        direction_deviation = 0.1,
-        speed = 0.1,
-        speed_deviation = 0.03,
-        center = {0, 0},
-        creation_distance = -1.925,
-        starting_frame_speed = 0.2,
-        starting_frame_speed_deviation = 0.1
+        category = "bullet",
+        action =
+        {
+          type = "direct",
+          action_delivery =
+          {
+            type = "instant",
+            source_effects =
+            {
+              type = "create-explosion",
+              entity_name = "explosion-gunshot"
+            },
+            target_effects =
+            {
+              {
+                type = "create-entity",
+                entity_name = "explosion-hit"
+              },
+              {
+                type = "damage",
+                damage = { amount = 5 , type = "physical"}
+              }
+            }
+          }
+        }
       },
-      range = 17,
+      range = 3,
       sound = make_heavy_gunshot_sounds(),
     },
   },
